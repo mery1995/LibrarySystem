7ab0fd56185d3ec663a4fc790592636af83f555f
mery1995 <marianna.jedruch@gmail.com>
2019-12-11 20:51:06 +0100

    Adding Person

---
+++ source/Person.py
@@ -0,0 +1,13 @@
+class Person:
+    first_name=''
+    last_name=''
+    pesel = 0
+    email = ''
+
+    def __init__(self, first_name:str, last_name:str, pesel:int, email:str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pesel = pesel
+        self.email = email
+
+
